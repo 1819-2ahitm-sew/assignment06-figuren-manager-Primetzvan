5c51fe2ac2ef8ea83d1b171beaf7433246aeafd4
Primetzvan <vaniprimetz@gmail.com>
2018-10-30 19:07:20 +0100

    Endabgabe

--- figuren_manager/src/main/java/Figure.java
+++ figuren_manager/src/main/java/Figure.java
@@ -55,17 +55,18 @@ abstract class Figure {
 
         @Override
         public double area() {
-            return Math.PI * getA() * getB();
+            return Math.round((Math.PI * getA() * getB()) * 1000 ) / 1000.0;
         }
 
         @Override
         public double circumference() {
-            return 0;
+            int lamba = (getA() - getB())/(getA()+getB());
+            return Math.round(((getA()+getB()) * Math.PI * (1 +( 3 * lamba * lamba ) / ( 10 + Math.pow(4 - 3 * lamba * lamba,2) )) )* 1000 ) / 1000.0 ;
         }
 
         @Override
         public String output() {
-            return  " Fläche -> " + area() + ", Umfang -> " + circumference();
+            return  "Ellipse mit Hauptachse " + this.getA() + " und Nebenachse " + this.getB() + ": Fläche -> " + area() + ", Umfang -> " + circumference();
         }
 
 
@@ -79,17 +80,17 @@ class Circle extends Figure {
 
     @Override
     public double area() {
-        return Math.PI * getA() * getA();
+        return Math.round((Math.PI * getA() * getA()) * 1000 ) / 1000.0;
     }
 
     @Override
     public double circumference() {
-        return 2 * Math.PI * getA();
+        return Math.round((2 * Math.PI * getA()) * 1000 ) / 1000.0;
     }
 
     @Override
     public String output() {
-        return " Fläche -> " + area() + ", Umfang -> " + circumference();
+        return "Kreis mit Radius " + this.getA() + ": Fläche -> " + area() + ", Umfang -> " + circumference();
     }
 
 
@@ -103,17 +104,17 @@ class Circle extends Figure {
 
     @Override
     public double area() {
-        return (getA() * getB() * Math.sin(getC()) / 2) ;
+        return Math.round(((getA() * getB() * Math.sin(getC()) / 2)) * 1000 ) / 1000.0 ;
     }
 
     @Override
     public double circumference() {
-        return Math.sqrt(getA() * getA() + getB() * getB() - (2* getA() * getB()) * Math.cos(getC()));
+        return Math.round((Math.sqrt(getA() * getA() + getB() * getB() - (2* getA() * getB()) * Math.cos(getC())) ) * 1000 ) / 1000.0;
     }
 
     @Override
     public String output() {
-        return " Fläche -> " + area() + ", Umfang -> " + circumference();
+        return "Dreieck mit Seite 1: " + this.getA() + " Seite 2: " + this.getB() + " und eingeschlossenem Winkel " + this.getC() + ": Fläche -> " + area() + ", Umfang -> " + circumference();
     }
 
 
@@ -127,17 +128,17 @@ class Square extends Figure{
 
     @Override
     public double area() {
-        return getA() * getA();
+        return Math.round((getA() * getA()) * 1000 ) / 1000.0;
     }
 
     @Override
     public double circumference() {
-        return getA() *4;
+        return Math.round((getA() *4) * 1000 ) / 1000.0;
     }
 
     @Override
     public String output() {
-       return " Fläche -> " + area() + ", Umfang -> " + circumference();
+       return "Quadrat mit Seitenlänge " + this.getA() + ": Fläche -> " + area() + ", Umfang -> " + circumference();
     }
 
 
@@ -151,17 +152,17 @@ class Rectangle extends Figure{
 
     @Override
     public double area() {
-        return getA() * getB();
+        return Math.round((getA() * getB()) * 1000 ) / 1000.0;
     }
 
     @Override
     public double circumference() {
-        return (getA() + getB())* 2;
+        return Math.round(((getA() + getB())* 2) * 1000 ) / 1000.0;
     }
 
     @Override
     public String output() {
-        return  " Fläche -> " + area() + ", Umfang -> " + circumference();
+        return  "Rechteck mit Seite 1: " + this.getA() + " und Seite 2: " + this.getB() + ": Fläche -> " + area() + ", Umfang -> " + circumference();
     }
 
 }
--- figuren_manager/src/main/java/Main.java
+++ figuren_manager/src/main/java/Main.java
@@ -8,65 +8,92 @@ public class Main {
         int eingabe1;
         int eingabe2;
         int eingabe3;
+        String allfigures[] = new String[100];
+        int i = 0;
         Figure figure;
 
-        System.out.println("Welche Figur wollen sie berechnet haben? (Geben sie nur den Anfangsbuchstaben ein!)");
+        System.out.println("Welche Figur wollen sie berechnet haben? (Geben sie nur den Anfangsbuchstaben ein!) ");
+        System.out.println("C = close; E = Ellipse; Q = Quadrat; K = Kreis; D = Dreieck; R = Rechteck; A = Ausgabe");
         form = sc.next().charAt(0);
 
+        while(form != 'c' && form != 'C' ){
 
         switch (form) {
             case 'e':
             case 'E':
-                System.out.print("Ellipse mit Hauptachse ");
+                System.out.print("Ellipse mit Hauptachse: ");
                 eingabe1 = sc.nextInt();
-                System.out.print(" und Nebenachse ");
+                System.out.print("Nebenachse: ");
                 eingabe2 = sc.nextInt();
-                System.out.print(":");
                 figure = new Ellipse(eingabe1, eingabe2);
-                System.out.print(figure.output());
+                allfigures [i] = figure.output();
+                i++;
                 break;
             case 'q':
             case 'Q':
-                System.out.print("Quadrat mit Seitenlänge ");
+                System.out.print("Quadrat mit Seitenlänge: ");
                 eingabe1 = sc.nextInt();
-                System.out.print(":");
                 figure = new Square(eingabe1);
-                System.out.println(figure.output());
+                allfigures [i] = figure.output();
+                i++;
                 break;
             case 'k':
             case 'K':
-                System.out.print("Kreis mit Radius ");
+                System.out.print("Kreis mit Radius: ");
                 eingabe1 = sc.nextInt();
-                System.out.print(":");
                 figure = new Circle(eingabe1);
-                System.out.println(figure.output());
+                allfigures [i] = figure.output();
+                i++;
                 break;
             case 'd':
             case 'D':
-                System.out.print("Dreieck mit Seite 1 ");
+                System.out.print("Dreieck mit Seite 1: ");
                 eingabe1 = sc.nextInt();
-                System.out.print(", Seite 2 ");
+                System.out.print("Seite 2: ");
                 eingabe2 = sc.nextInt();
-                System.out.print(" und eingeschlossenem Winkel ");
+                System.out.print("und eingeschlossenem Winkel: ");
                 eingabe3 = sc.nextInt();
-                System.out.print(":");
                 figure = new Triangle(eingabe1, eingabe2, eingabe3);
-                System.out.println(figure.output());
+                allfigures [i] = figure.output();
+                i++;
                 break;
             case 'r':
             case 'R':
-                System.out.print("Rechteck mit Seite 1 ");
+                System.out.print("Rechteck mit Seite 1: ");
                 eingabe1 = sc.nextInt();
-                System.out.print(", Seite 2 ");
+                System.out.print("Seite 2: ");
                 eingabe2 = sc.nextInt();
-                System.out.print(":");
                 figure = new Rectangle(eingabe1, eingabe2);
-                System.out.println(figure.output());
+                allfigures [i] = figure.output();
+                i++;
+                break;
+            case 'a':
+            case 'A':
+
+                System.out.printf("\nDatenanzeige: \n-------------\n");
+
+                for (int j = 0; j < i; j++) {
+                    System.out.println(allfigures[j]);
+                }
+                System.out.println();
+                break;
+            default:
+                System.out.println("Falsche Eingabe!");
+                System.out.println("C = close; E = Ellipse; Q = Quadrat; K = Kreis; D = Dreieck; R = Rechteck; A = Ausgabe");
         }
 
+            System.out.println("Welche Figur wollen sie berechnet haben? (Geben sie nur den Anfangsbuchstaben ein!)");
+            form = sc.next().charAt(0);
 
     }
 
+        System.out.printf("\nDatenanzeige: \n-------------\n");
+
+        for (int j = 0; j < i; j++) {
+            System.out.println(allfigures[j]);
+        }
+
+}
 
 }
 
